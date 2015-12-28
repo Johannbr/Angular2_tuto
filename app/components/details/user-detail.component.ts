23846dab626b7b0592a575b86a44a6d459d66d60
JohannBr <johannbr@gmail.com>
2015-12-28 12:28:40 +0100

    Replace inputs:[] parameter in UserDetailComponent by @Input() properties

--- app/components/details/user-detail.component.ts
+++ app/components/details/user-detail.component.ts
@@ -1,24 +1,23 @@
-import {Component} from 'angular2/core';
+import {Component,Input} from 'angular2/core';
 import {User} from '../../shared/user/user';
 import {UserAutreDetailComponent} from '../edit_detail/user-edit-detail.component';
 import {UserManager} from 'app/shared/user/user-manager';
 @Component({
   selector: 'my-user-detail',
-  templateUrl:'app/components/details/user-detail.template.html' ,
-  inputs: ['user']
+  templateUrl:'app/components/details/user-detail.template.html'
 })
 export class UserDetailComponent {
-  // public user: User;
+  @Input('my-user') user:string;
   public editing:boolean;
   constructor(public um:UserManager){
 this.editing=false;
-// console.log(this.user)
 }
   edit(){
     this.editing = true;
     return false;
   }
   save(user){
+    console.log(user);
     this.um.saveUser(user);
     this.editing = false;
     return false;
